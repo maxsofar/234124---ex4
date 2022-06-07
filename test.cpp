4250a153536f8ef02d4b135cb6fda4eeea9ea972
maxsofar <maxshermant@campus.technion.ac.il>
2022-06-07 11:56:14 +0300

    Added include to <cstring>

--- test.cpp
+++ test.cpp
@@ -15,41 +15,52 @@
 #include "Cards/Merchant.h"
 #include "Cards/Pitfall.h"
 #include "Cards/Treasure.h"
+#include "Players/Rogue.h"
+#include "Players/Wizard.h"
+#include "Players/Fighter.h"
+#include "Exception.h"
 #include <algorithm>
 #include <sstream>
 #include <random>
 #include <cstdio>
 #include <fstream>
+#include <cstring>
 
 using std::unique_ptr;
 using std::cout;
+using std::cerr;
+using std::string;
+using std::fstream;
+using std::istringstream;
+using std::vector;
+
 
 /* ---------------------------------------------------------------------------------------------- */
-
 // --------------------------------       General Helper Functions          ------------------------------
 
-
-void createTextFile(const string filename, const string input){
+void createTextFile(const string &filename, const string &input)
+{
     std::ofstream file(filename);
     if(file){
         file << input;
     }
 }
 
-void deleteTextFile(const string filename){
+void deleteTextFile(const string &filename)
+{
     std::remove(filename.c_str());
 }
 
-bool compareFiles(string filename1, string filename2)
+bool compareFiles(const string &filename1, const string &filename2)
 {
     string line1,line2;
     fstream file1(filename1),file2(filename2);
     if( !file2){
-         cerr<<"Error opening file 2"<<endl;
+         cerr<<"Error opening file 2"<<std::endl;
          return false;
     }
 	if(!file1 ){
-         cerr<<"Error opening file 1"<<endl;
+         cerr<<"Error opening file 1"<<std::endl;
          return false;
     }
     while(!file1.eof()){ //read file until you reach the end
@@ -66,14 +77,15 @@ bool compareFiles(string filename1, string filename2)
     return true;
 }
 
-bool GeneralGameSimulationTest(const string tempDeckFilename, string input, string deck, string expectedOutputFileName){
-//   init cin from file
+bool GeneralGameSimulationTest(const string &tempDeckFilename, string input, string deck, string expectedOutputFileName)
+{
+    //   init cin from file
     createTextFile(tempDeckFilename+".txt",deck);
     istringstream in(input);
     std::streambuf *cinbuf = std::cin.rdbuf(); //save old buf
     std::cin.rdbuf(in.rdbuf());
 
-    ofstream outfile(tempDeckFilename+"out.txt");
+    std::ofstream outfile(tempDeckFilename+"out.txt");
     std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
     std::cout.rdbuf(outfile.rdbuf());
     Mtmchkin game(tempDeckFilename+".txt");
@@ -87,29 +99,8 @@ bool GeneralGameSimulationTest(const string tempDeckFilename, string input, stri
     std::cin.rdbuf(cinbuf);
     std::cout.rdbuf(coutbuf);
     deleteTextFile(tempDeckFilename+".txt");
-//    deleteTextFile(tempDeckFilename+"out.txt");
     return res;
 }
-
-// bool GeneralGameSimulationTest(const string tempFilename, string input, string expectedOutputFile){
-// //   init cin from file
-//     istringstream in(input);
-//     std::streambuf *cinbuf = std::cin.rdbuf(); //save old buf
-//     std::cin.rdbuf(in.rdbuf());
-
-//     ofstream outfile(tempFilename+"out.txt");
-//     std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
-//     std::cout.rdbuf(outfile.rdbuf());
-//     Mtmchkin game(tempFilename+".txt");
-//     while(!game.isGameOver() && game.getNumberOfRounds() < 100){
-//         game.playRound();
-//         game.printLeaderBoard();
-//     }
-//     bool res = compareFiles(tempFilename+"out.txt", expectedOutputFile);
-//     std::cin.rdbuf(cinbuf);
-//     std::cout.rdbuf(coutbuf);
-//     return res;
-// }
 
 void run_test(std::function<bool()> test, std::string test_name)
 {
@@ -125,12 +116,10 @@ void run_test(std::function<bool()> test, std::string test_name)
 
 
 /* ---------------------------------------------------------------------------------------------- */
-
 // --------------------------------       Tests for Card class          ------------------------------
 
-
-
-bool cardsPrintsTest(){
+bool cardsPrintsTest()
+{
     
     Barfight junta;
     Dragon mushu;
@@ -147,8 +136,8 @@ bool cardsPrintsTest(){
     return true;
 }
 
-
-bool playersPrintsTest(){
+bool playersPrintsTest()
+{
     
     Rogue player1("Itay");
     Fighter player2("Efrat");
@@ -176,20 +165,16 @@ bool testCard()
     return true;
 }
 
-
 /* ---------------------------------------------------------------------------------------------- */
-
 // --------------------------------       Tests for Mtmchkin class          ------------------------------
-
 
 bool gameRunTest(){
 //   init cin from file
     std::ifstream in("in.txt");
     if(!in.is_open()){
-        throw exception();
+        throw std::exception();
     }
     std::cin.rdbuf(in.rdbuf());
-
     Mtmchkin game("gametest.txt");
     while(!game.isGameOver()){
         game.playRound();
@@ -276,7 +261,6 @@ bool merchantInputTest()
 }
 
 /* ---------------------------------------------------------------------------------------------- */
-
 // --------------------------------       Tests for Exceptions          ------------------------------
 
 bool badSizeTest()
@@ -294,6 +278,7 @@ bool badSizeTest()
     }
     return flag;
 }
+
 bool noFileTest()
 {
     const string tmp_file("noFile_test");
@@ -309,6 +294,7 @@ bool noFileTest()
     }
     return flag;
 }
+
 bool badFormatTest()
 {
     const string tmp_file("badFormat_test");
@@ -327,6 +313,7 @@ bool badFormatTest()
     }
     return flag;
 }
+
 bool badFormatStartTest()
 {
     const string tmp_file("badFormat_test");
@@ -347,11 +334,10 @@ bool badFormatStartTest()
 }
 
 /* ---------------------------------------------------------------------------------------------- */
-
 // --------------------------------       Main function          ------------------------------
 
-
 int main(){
+    
 	run_test(cardsPrintsTest,"cardsPrintsTest");
 	run_test(playersPrintsTest,"playersPrintsTest");
 	run_test(testCard,"Deck creation test");
@@ -367,19 +353,6 @@ int main(){
     run_test(allTenTest,"All reach lvl 10 test");
     run_test(badPlayerInputTest,"Bad player input test");
     run_test(merchantInputTest,"Merchant input test");
-
-
-
-
-
-   //Mtmchkin game("dragonDen_test.txt");
-   //    while(!game.isGameOver())
-   //    {
-   //        game.playRound();
-   //        game.printLeaderBoard();
-   //    }
-
-
 
     return 0;
 }
