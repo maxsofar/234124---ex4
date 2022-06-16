a432ba632b92fd3b425626155d049adab8105c5d
Max <maxshermant@campus.technion.ac.il>
2022-06-16 17:17:04 +0300

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "Mtmchkin.h"
+
+int main() {
+    try{
+        Mtmchkin game("deck.txt");
+        while(!game.isGameOver() && game.getNumberOfRounds() < 100){
+            game.playRound();
+            game.printLeaderBoard();
+        }
+    }
+    catch(const DeckFileInvalidSize& error){
+        std::cout << error.what()  << std::endl;
+    }
+    catch(const DeckFileNotFound& error){
+        std::cout << error.what()  << std::endl;
+    }
+    catch(const DeckFileFormatError& error){
+        std::cout << error.what()  << std::endl;
+    }
+    return 0;
+}
