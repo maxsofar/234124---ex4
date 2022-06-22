8e69430719513522164616feb8fad3c49a4bc6fe
Max <maxshermant@campus.technion.ac.il>
2022-06-22 10:18:08 +0300

    more tweaks

--- Cards/Merchant.cpp
+++ Cards/Merchant.cpp
@@ -10,47 +10,50 @@ using std::string;
 Merchant::Merchant() : Card(CardStats (), "Merchant")
 {}
 
-void Merchant::applyEncounter(Player &player) const
+static int getInput()
 {
-    printMerchantInitialMessageForInteractiveEncounter(std::cout, player.getName(), player.getCoins());
     string input;
-    int intInput;
     while(true) {
         getline(cin, input);
         if (cin.fail() || cin.eof() || input.empty() || input.find_first_not_of(AVAILABLE_CHOICES) != string::npos) {
             printInvalidInput();
         } else {
-            intInput = std::stoi(input);
-            break;
+            return std::stoi(input);
         }
     }
-    switch (intInput) {
+}
+
+void Merchant::applyEncounter(Player &player) const
+{
+    printMerchantInitialMessageForInteractiveEncounter(std::cout, player.getName(), player.getCoins());
+    int input = getInput();
+    switch (input) {
         case 0:
         {
-            printMerchantSummary(std::cout, player.getName(), 0, 0);
+            printMerchantSummary(std::cout, player.getName(), input, 0);
         }
             break;
 
         case 1:
         {
-            if (player.pay(5)) {
-                player.heal(1);
-                printMerchantSummary(std::cout, player.getName(), 1, 5);
+            if (player.pay(HP_BUFF_COST)) {
+                player.heal(HP_BUFF_AMOUNT);
+                printMerchantSummary(std::cout, player.getName(), input, HP_BUFF_COST);
             } else {
                 printMerchantInsufficientCoins(std::cout);
-                printMerchantSummary(std::cout, player.getName(), 1, 0);
+                printMerchantSummary(std::cout, player.getName(), input, 0);
             }
         }
             break;
 
         case 2:
         {
-            if (player.pay(10)) {
-                player.buff(1);
-                printMerchantSummary(std::cout, player.getName(), 2, 10);
+            if (player.pay(FORCE_BUFF_COST)) {
+                player.buff(FORCE_BUFF_AMOUNT);
+                printMerchantSummary(std::cout, player.getName(), input, FORCE_BUFF_COST);
             } else {
                 printMerchantInsufficientCoins(std::cout);
-                printMerchantSummary(std::cout, player.getName(), 2, 0);
+                printMerchantSummary(std::cout, player.getName(), input, 0);
             }
         }
             break;
