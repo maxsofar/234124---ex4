9ea12e6ecfa213364d6397174dbb8af50bac735b
Max <maxshermant@campus.technion.ac.il>
2022-06-23 17:23:11 +0300

    added players input

--- tool/main.cpp
+++ tool/main.cpp
@@ -1,6 +1,12 @@
 #include "Mtmchkin.h"
+#include <string>
 
 int main() {
+    std::string input("3\nJonSnow Fighter\nAlbusDumbledore Wizard\nJackSparrow Rogue");
+    std::istringstream in(input);
+    std::streambuf *cinbuf = std::cin.rdbuf();
+    std::cin.rdbuf(in.rdbuf());
+
     try{
         Mtmchkin game("../deck.txt");
         while(!game.isGameOver() && game.getNumberOfRounds() < 100){
@@ -11,6 +17,7 @@ int main() {
     catch(const Exception& error){
         std::cout << error.what()  << std::endl;
     }
+    std::cin.rdbuf(cinbuf);
 
     return 0;
 }
